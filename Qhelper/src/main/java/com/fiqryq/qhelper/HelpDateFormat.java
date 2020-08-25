f78e820aa31329c70f971e73ab928f67a41d0f90
Fiqry Choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-08-25 12:45:38 +0700

    init

---
+++ Qhelper/src/main/java/com/fiqryq/qhelper/HelpDateFormat.java
@@ -0,0 +1,39 @@
+package com.fiqryq.qhelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class HelpDateFormat {
+
+    public static String getNameDay() {
+        Locale id = new Locale("in", "ID");
+        String pattern = "EEEE";
+        Date today = new Date();
+
+        // Gets formatted date specify by the given pattern for
+        // Indonesian Locale no changes for default date format
+        // is applied here.
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, id);
+        return sdf.format(today);
+    }
+
+    public static String getTimeNow() {
+        Locale id = new Locale("in", "ID");
+        String pattern = "EEEE, dd MMMM yyyy";
+        Date today = new Date();
+
+        // Gets formatted date specify by the given pattern for
+        // Indonesian Locale no changes for default date format
+        // is applied here.
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, id);
+        return sdf.format(today);
+    }
+
+    public static String getHour(){
+        Date dateNow = Calendar.getInstance().getTime();
+        String hour = (String) android.text.format.DateFormat.format("HH:mm", dateNow);
+        return hour;
+    }
+}
---
+++ app/src/main/java/com/fiqryq/qhelper/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fiqryq.qhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
